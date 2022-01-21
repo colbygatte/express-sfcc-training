ca78af1e956b0d6de49b281787127c4b3ff6037e
Colby Gatte <colbygatte@gmail.com>
2022-01-20 18:28:58 -0600

    Add jQuery

--- client/index.js
+++ client/index.js
@@ -1,11 +1,5 @@
 // Remove the following lines, if you do not need any of Bootstrap's JavaScript features
 import Popper from "popper.js";
-window.jQuery = $;
-window.$ = $;
+window.$ = window.jQuery = require('jquery');
 
-require("bootstrap");
-
-// Remove this demo code, that's only here to show how the .env file works!
-if (process.env["HELLO"]) {
-  console.log(`Hello ${process.env.HELLO}!`);
-}
+require('bootstrap');
